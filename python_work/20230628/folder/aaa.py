bb539fbf9fd59f6efc9799e3240ad55e68ecd684
dron512 <parkmyounghoi@gmail.com>
2023-06-29 09:40:42 +0900

    vscode upload

---
+++ python_work/20230628/folder/aaa.py
@@ -0,0 +1,3 @@
+def doA():
+    print("doA")
+
---
+++ python_work/20230628/test.py
@@ -0,0 +1,22 @@
+result = ['bream']*35+['smelt']*14
+print(result)
+
+# a = [1,2,3,4,5]
+# b = [4,5,6,7,10]
+
+# print(a+b)
+
+# for x,y in zip(a,b):
+#     print(x,y)
+
+# c = [[x,y] for x,y in zip(a,b)]
+# print(c[0])
+# print(c[1])
+# print(c[2])
+
+class AA:
+    def doA(self):
+        print("aa")
+
+aa = AA()
+aa.doA()
