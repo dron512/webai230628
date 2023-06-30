6bc1043640ca19689d00fbf96c51667f213217f1
dron512 <parkmyounghoi@gmail.com>
2023-06-30 14:06:50 +0900

    230630

--- springboot_work/demo/src/main/java/com/example/demo/controller/MainHtmlController.java
+++ springboot_work/demo/src/main/java/com/example/demo/controller/MainHtmlController.java
@@ -1,13 +1,29 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainHtmlController {
 
     @GetMapping("aa")
-    public String aa(){
+    public String aa(
+            Model model,
+            String num1,
+            String num2){
+        System.out.println("출력");
+        System.out.println(num1);
+        System.out.println(num2);
+        if (num1 == null) num1 = "0";
+        if (num2 == null) num2 = "0";
+        model.addAttribute(
+                "data",
+                "asdfasdf");
+        model.addAttribute(
+                "result",
+                Integer.parseInt(num1)+
+                        Integer.parseInt(num2));
         return "aa";
     }
 }
