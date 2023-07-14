bbf71a2c126c41ecee323721aeb1626bdd35b2b3
dron512 <parkmyounghoi@gmail.com>
2023-07-14 16:09:00 +0900

    python

--- python_work/pythonProject/230713/ex02.py
+++ python_work/pythonProject/230713/ex02.py
@@ -1,18 +1,80 @@
-from flask import Flask, request
+from flask import Flask, send_file, request, render_template, make_response
+
+from io import BytesIO, StringIO
+import numpy as np
+
+## macOS의 경우 아래 순서에 따라서 library를 import해줘야 에러없이 잘 됩니다.
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 from ex01 import rfclf
+from flask_cors import CORS
+from functools import wraps, update_wrapper
+from datetime import datetime
 
 app = Flask(__name__)
+
+CORS(app)
+
+
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+
+    return update_wrapper(no_cache, view)
+
 
 @app.route("/")
 def index():
     alcohol = request.args.get("alcohol")
     sugar = request.args.get("sugar")
     pH = request.args.get("pH")
+    print(f"alcohol = {alcohol} sugar={sugar} pH={pH}")
 
-    if alcohol :
-        predValue = rfclf.predict([[float(alcohol),float(sugar),float(pH)]])
+    if alcohol:
+        predValue = rfclf.predict([[float(alcohol), float(sugar), float(pH)]])
         return str(predValue)
     else:
         return "예측할값을 입력하세요"
 
-app.run(debug=True)
+
+@app.route('/fig/<int:mean>_<int:var>')
+@nocache
+def fig(mean, var):
+    plt.figure(figsize=(4, 3))
+    xs = np.random.normal(mean, var, 100)
+    ys = np.random.normal(mean, var, 100)
+    plt.scatter(xs, ys, s=100, marker='h', color='red', alpha=0.3)
+    """
+    file로 저장하는 것이 아니라 binary object에 저장해서 그대로 file을 넘겨준다고 생각하면 됨
+    """
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=100)
+    img.seek(0)  ## object를 읽었기 때문에 처음으로 돌아가줌
+    plt.close()
+    return send_file(img, mimetype='image/png')
+    # plt.savefig(img, format='svg')
+    # return send_file(img, mimetype='image/svg')
+
+
+@app.route('/fie/<int:a>_<int:b>_<int:c>_<int:d>')
+@nocache
+def fie(a, b, c, d):
+    ratio = [a, b, c, d]
+    labels = ['Apple', 'Banana', 'Melon', 'Grapes']
+    plt.pie(ratio, labels=labels, autopct='%.1f%%')
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=100)
+    img.seek(0)
+    plt.close()
+    return send_file(img, mimetype='image/png')
+
+app.run(debug=True, host="0.0.0.0")
---
+++ python_work/pythonProject/230713/ex03.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fruits = np.load('fruits_300.npy')
+
+print(fruits.shape)
+
+# plt.figure(figsize=(2,2))
+# 0 0 0 -> 검정색
+# 255 255 255 -> 하얀색
+
+
+
+apple = fruits[0:100].reshape(-1,100*100)
+pineapple = fruits[100:200].reshape(-1,100*100)
+banana = fruits[200:300].reshape(-1,100*100)
+
+print(apple[0])
+print(apple.shape)
+
+print(apple.mean(axis=1))
+print(pineapple.mean(axis=1))
+print(banana.mean(axis=1))
+
+# dopaint(apple,pineapple,banana)
+
+plt.hist(np.mean(apple,axis=1),alpha=0.8)
+plt.hist(np.mean(pineapple,axis=1),alpha=0.8)
+plt.hist(np.mean(banana,axis=1),alpha=0.8)
+plt.show()
+
+# fig,axis = plt.subplots(1,3,figsize=(20,5))
+#
+# axis[0].bar(range(10000),np.mean(apple,axis=0),alpha=0.8)
+# axis[1].bar(range(10000),np.mean(pineapple,axis=0),alpha=0.8)
+# axis[2].bar(range(10000),np.mean(banana,axis=0),alpha=0.8)
+# plt.show()
+
+def dopaint(apple,pineapple,banana):
+    fig,axis = plt.subplots(1,3)
+    axis[0].imshow(apple.reshape(-1,100),cmap='gray')
+    axis[1].imshow(pineapple.reshape(-1,100),cmap='gray')
+    axis[2].imshow(banana.reshape(-1, 100), cmap='gray')
+    plt.show()
+
+# dopaint(apple.mean(axis=0),
+#         pineapple.mean(axis=0),
+#         banana.mean(axis=0))
+
+apple_mean = apple.mean(axis=0).reshape(100,100)
+pineapple_mean = pineapple.mean(axis=0).reshape(100,100)
+banana_mean = banana.mean(axis=0).reshape(100,100)
+
+abs_diff = np.abs(fruits-banana_mean)
+print(abs_diff.shape)
+abs_mean = np.mean(abs_diff,axis=(1,2))
+
+apple_index = np.argsort(abs_mean)[:100]
+print(apple_index)
+fig,axis = plt.subplots(10,10,figsize=(10,10))
+for i in range(10):
+    for j in range(10):
+        axis[i,j].imshow(fruits[apple_index[i*10+j]],cmap='gray_r')
+plt.show()
+
+
+
+
+
