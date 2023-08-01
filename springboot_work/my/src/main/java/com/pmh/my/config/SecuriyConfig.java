3d68ce831e8b9eb66710f33a24a9b93d19b24f66
dron512 <parkmyounghoi@gmail.com>
2023-08-01 12:08:15 +0900

    230801

--- springboot_work/my/src/main/java/com/pmh/my/config/SecuriyConfig.java
+++ springboot_work/my/src/main/java/com/pmh/my/config/SecuriyConfig.java
@@ -1,7 +1,13 @@
 package com.pmh.my.config;
 
+import java.util.Date;
 import java.util.List;
 
+import com.pmh.my.MyApplication;
+import com.pmh.my.service.CustomOauth2UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,6 +27,9 @@ import lombok.RequiredArgsConstructor;
 public class SecuriyConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+    private final CustomOauth2UserService oAuth2UserService;
+
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -41,13 +50,45 @@ public class SecuriyConfig {
                             authorizeRequest
                                     // 회원가입 로그인 허용...
                                     .requestMatchers("/auth/**").permitAll()
+                                    // user로 요청하는 것은 허용
+                                    .requestMatchers("/user/**").permitAll()
                                     // 다른것들은 인증이 필요하다...
+                                    // 인증이 필요하기 때문에 google 로그인 페이지로 이동된다.
                                     .anyRequest().authenticated()
                         )
+                .oauth2Login((oauth2Login) -> oauth2Login
+                        .userInfoEndpoint((userInfo) -> userInfo
+                                .userService(oAuth2UserService)
+                        )
+                        .successHandler(
+                            (request,response,authentication)->{
+                                // 응답 하는게 문자열이다.
+                                response.setContentType("text/html;charset=UTF-8");
+                                Claims claims = Jwts.claims();
+                                claims.put("username", "pmh");
+
+                                String mytoken = Jwts.builder()
+                                        // username 도 추가 해서 암호화 해줘
+                                        .setClaims(claims)
+                                        // 현재 시간으로 발행되었다.
+                                        .setIssuedAt(new Date(System.currentTimeMillis()))
+                                        // 5분뒤에 사용할수 없다.
+                                        .setExpiration(new Date(System.currentTimeMillis() + 1000*60*5l))
+                                        // KEY 값으로 암호화 해라
+                                        .signWith(SignatureAlgorithm.HS512, MyApplication.KEY)
+                                        .compact();
+
+                                var writer = response.getWriter();
+                                writer.println(mytoken);
+                            }
+                        )
+                )
+//                .formLogin(fo->fo.loginPage())
                 // session 방식 안하고 JWT 할 계획임
                 .sessionManagement(ss -> ss.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // Username 체크하기 전에 jwtAuthFilter 객체 들어가야함.
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                
         return http.build();
     }
 }
--- springboot_work/my/src/main/java/com/pmh/my/controller/UserController.java
+++ springboot_work/my/src/main/java/com/pmh/my/controller/UserController.java
@@ -1,2 +1,21 @@
-package com.pmh.my.controller;public class UserController {
+package com.pmh.my.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    // 로그인 하면 Authentication 객체 만들어진
+    @GetMapping("user")
+    public String user(Authentication authentication){
+        System.out.println("test");
+
+        System.out.println(authentication.getPrincipal());
+        return authentication.getName();
+    }
 }
